bba5ee5ec918931fa83e876cac43900e753f07a9
Marsot Pierre <pierre.mmarsot@gmail.com>
2024-06-21 16:13:55 +0200

    add 3 stakedao strats

---
+++ integrations/stakedao_usde_july.py
@@ -0,0 +1,11 @@
+from constants.integration_ids import IntegrationID
+from utils.stakedao import StakeDAOIntegration
+from constants.stakedao import USDE_JULY, USDE_JULY_DEPLOYMENT_BLOCK  
+
+if __name__ == "__main__":
+    stakedao_integration = StakeDAOIntegration(
+        IntegrationID.STAKEDAO_USDE_JULY_LPT,
+        USDE_JULY_DEPLOYMENT_BLOCK,
+        USDE_JULY,
+    )
+    print(stakedao_integration.get_participants())
