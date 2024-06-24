422cb85df86c9cc38d4ec8e40d81731fc1bbcebb
b <b@ethenalabs.xyz>
2024-06-23 20:39:21 -0400

    excluded_addresses

--- models/integration.py
+++ models/integration.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import List
 
 from constants.chains import Chain
 from constants.integration_ids import IntegrationID
@@ -8,9 +9,18 @@ from constants.integration_token import Token
 
 class Integration(ABC):
 
-    def __init__(self, integration_id: IntegrationID, start_block: int, chain: Chain, summary_cols: list[SummaryColumn],
-                 reward_multiplier: int = 1, balance_multiplier: int = 1, end_block: int = None,
-                 reward_multiplier_func=None):
+    def __init__(
+        self,
+        integration_id: IntegrationID,
+        start_block: int,
+        chain: Chain,
+        summary_cols: list[SummaryColumn],
+        reward_multiplier: int = 1,
+        balance_multiplier: int = 1,
+        excluded_addresses: List[str] = None,
+        end_block: int = None,
+        reward_multiplier_func=None,
+    ):
         self.integration_id = integration_id
         self.start_block = start_block
         self.end_block = end_block
@@ -19,6 +29,7 @@ class Integration(ABC):
         self.summary_cols = summary_cols
         self.reward_multiplier = reward_multiplier
         self.balance_multiplier = balance_multiplier
+        self.excluded_addresses = excluded_addresses
         self.reward_multiplier_func = reward_multiplier_func
 
     @abstractmethod
@@ -60,8 +71,13 @@ class Integration(ABC):
 
     def get_end_block(self) -> int:
         if self.end_block is None:
-            return 2 ** 31 - 1  # if no end block is specified, return the maximum possible block number
+            return (
+                2**31 - 1
+            )  # if no end block is specified, return the maximum possible block number
         return self.end_block
+
+    def get_excluded_addresses(self) -> List[str]:
+        return self.excluded_addresses
 
     def is_user_a_participant(self, user: str) -> bool:
         if self.participants is None:
