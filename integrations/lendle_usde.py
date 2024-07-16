7c246370bc6efcefc7054460f9739635f00aec23
ivdznk <dzizenko.i@gmail.com>
2024-07-16 15:21:11 +0300

    Lendle adapter updates

--- integrations/lendle_usde.py
+++ integrations/lendle_usde.py
@@ -2,7 +2,7 @@ from constants.chains import Chain
 from constants.integration_ids import IntegrationID
 from models.integration import Integration
 from constants.summary_columns import SummaryColumn
-from constants.lendle import LENDLE_DEPLOYMENT_BLOCK
+from constants.lendle import LENDLE_USDE_DEPLOYMENT_BLOCK
 from utils.web3_utils import w3_mantle, fetch_events_logs_with_retry, call_with_retry
 from utils.lendle import lendle_usde_contract
 
@@ -12,9 +12,9 @@ class LendleIntegration(
     def __init__(self):
         super().__init__(
             IntegrationID.LENDLE_USDE_LP,
-            LENDLE_DEPLOYMENT_BLOCK,
+            LENDLE_USDE_DEPLOYMENT_BLOCK,
             Chain.MANTLE,
-            [SummaryColumn.LENDLE_MANTLE_SHARDS],  # TODO: Optional, change None to a list of SummaryColumn enums that this protocol should be associated with, see Pendle grouping example
+            [SummaryColumn.LENDLE_MANTLE_SHARDS],
             20,
             1,
             None,
@@ -23,22 +23,18 @@ class LendleIntegration(
 
     def get_balance(self, user: str, block: int) -> float:
         bal = call_with_retry(
-            lendle_usde_contract.balanceOf(user),
+            lendle_usde_contract.functions.balanceOf(user),
             block,
         )
         if bal == 0:
             return 0
-
-        print(
-            f"lendle_bal: {bal}"
-        )
 
         return round((bal / 10**18), 4)
 
     # Important: This function should only be called once and should cache the results by setting self.participants
     def get_participants(self) -> list:
         page_size = 1900
-        start_block = LENDLE_DEPLOYMENT_BLOCK
+        start_block = LENDLE_USDE_DEPLOYMENT_BLOCK
         target_block = w3_mantle.eth.get_block_number()
 
         all_users = set()
@@ -60,11 +56,10 @@ class LendleIntegration(
 
 
 if __name__ == "__main__":
-    # TODO: Write simple tests for the integration
-    example_integration = LendleIntegration()
-    print(example_integration.get_participants())
+    lendle_integration = LendleIntegration()
+    print(lendle_integration.get_participants())
     print(
-        example_integration.get_balance(
-            list(example_integration.get_participants())[0], LENDLE_DEPLOYMENT_BLOCK
+        lendle_integration.get_balance(
+            list(lendle_integration.participants)[0], "latest"
         )
     )
