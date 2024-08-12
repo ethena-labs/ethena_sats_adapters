a9532c439290b4c1230f53f3db1958f3c7e6e8ac
jan-magpie <155635960+jan-magpie@users.noreply.github.com>
2024-08-12 05:34:59 +0000

    added 4 more pools

---
+++ integrations/penpie_usde_dec.py
@@ -0,0 +1,19 @@
+from constants.integration_ids import IntegrationID
+from utils.penpie import PENPIEIntegration
+from constants.penpie import USDe_26DEC2024, USDe_26DEC2024_DEPLOYMENT_BLOCK   
+from constants.chains import Chain
+from constants.penpie import PENDLE_LOCKER_ETHEREUM
+
+if __name__ == "__main__":
+    penpie_integration = PENPIEIntegration(
+        IntegrationID.PENPIE_USDe_26DEC2024_LPT,
+        USDe_26DEC2024_DEPLOYMENT_BLOCK,
+        USDe_26DEC2024,
+        Chain.ETHEREUM,
+        20,
+        1,
+        [PENDLE_LOCKER_ETHEREUM]
+
+    )
+    # print(penpie_integration.get_participants())
+    print(penpie_integration.get_balance("0xe95176DF139a93D706655B32Df087a97e212B78E", "latest"))
