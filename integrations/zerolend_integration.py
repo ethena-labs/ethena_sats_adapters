2d5018ab1d059bfb8faa42c295ead02141781e17
Elliot Wood <elliotreecewood@gmail.com>
2025-01-20 15:06:30 +0000

    Refactor integration

--- integrations/zerolend_integration.py
+++ integrations/zerolend_integration.py
@@ -1,38 +1,43 @@
 from typing import Callable, Dict, List, Optional, Set
 from constants.chains import Chain
-from constants.summary_columns import SummaryColumn
 from utils.web3_utils import w3
+from constants.summary_columns import SummaryColumn
 from integrations.cached_balances_integration import CachedBalancesIntegration
 from integrations.integration_ids import IntegrationID
 import requests
 
 ZEOLEND_API_URL = "https://api.zerolend.xyz"
 
+
 class ZerolendIntegration(CachedBalancesIntegration):
     def __init__(
         self,
+        integration_id: IntegrationID,
+        reward_multiplier: int,
+        token: str,
     ):
         super().__init__(
-            IntegrationID.ZEROLEND_USDE,
+            integration_id,
             20000000,  # not used
             Chain.ETHEREUM,
             None,
-            20,
+            reward_multiplier,
             1,
             None,
             None,
             None,
         )
 
+        self.token = token
     def get_balance(self, user: str, block: int) -> float:
         try:
-            url = f"{ZEOLEND_API_URL}/ethena"
-            params = {"token":"usde","address": str(user), "blockNo": str(block)}
-            response = requests.get(url, params=params)
+            url = f"{ZEOLEND_API_URL}/ethena"  # TODO: add api url
+            params = {"token": self.token, "address": str(user), "blockNo": str(block)}
+            response = requests.get(url, params=params)  # type: ignore
             print(response.json())
             data = response.json()
             asset_balance = data["data"]
-            return asset_balance 
+            return asset_balance
         except Exception as ex:
             print("Error getting balance for user %s: %s", user, ex)
             return 0
@@ -43,16 +48,21 @@ class ZerolendIntegration(CachedBalancesIntegration):
         This function should only be called once and should cache the results by setting self.participants
         """
         url = f"{ZEOLEND_API_URL}/ethena/participants"
-        params = {"token": "usde"}
+        params = {"token": self.token}
         response = requests.get(url, params=params)
         data = response.json()
         return data["data"]
 
 
 if __name__ == "__main__":
-    zerolend = ZerolendIntegration()
+    zerolend = ZerolendIntegration(IntegrationID.ZEROLEND_SUSDE, 5, "susde")
+    # zerolend = ZerolendIntegration(IntegrationID.ZEROLEND_USDE, 20, "usde")
     participants = zerolend.get_participants(None)
     print("participants", participants)
     currentBlock = w3.eth.get_block_number()
     if len(participants) > 0:
-        print(zerolend.get_balance(list(participants)[len(participants) - 1], currentBlock))
+        print(
+            zerolend.get_balance(
+                list(participants)[len(participants) - 1], currentBlock
+            )
+        )
