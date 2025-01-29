15547baa3dac3d0efedafb2b9249a0578e361ed4
heri16 <527101+heri16@users.noreply.github.com>
2025-01-29 09:24:49 +0700

    cork: quickadd susde

--- constants/cork.py
+++ constants/cork.py
@@ -39,8 +39,14 @@ PSM_CONTRACT_BY_CHAIN = {
     ),
 }
 
-# Contract or first pair deployment block heights
+# block height of first deployment of contract or pair
 PSM_USDE_START_BLOCK_BY_CHAIN = {
+    Chain.ETHEREUM: 21725765,
+    Chain.SEPOLIA: 7214863,
+}
+
+# block height of first deployment of contract or pair
+PSM_SUSDE_START_BLOCK_BY_CHAIN = {
     Chain.ETHEREUM: 21725765,
     Chain.SEPOLIA: 7214863,
 }
@@ -48,14 +54,12 @@ PSM_USDE_START_BLOCK_BY_CHAIN = {
 # See: https://docs.ethena.fi/solution-design/key-addresses
 # See: https://docs.ethena.fi/resources/testnet
 USDE_TOKEN_ADDRESS_FOR_L2 = Web3.to_checksum_address("0x5d3a1Ff2b6BAb83b63cd9AD0787074081a52ef34")
-USDE_TOKEN_ADDRESS_BY_CHAIN = defaultdict(
-    lambda: USDE_TOKEN_ADDRESS_FOR_L2, {
+USDE_TOKEN_ADDRESS_BY_CHAIN = defaultdict(lambda: USDE_TOKEN_ADDRESS_FOR_L2, {
     Chain.ETHEREUM: Web3.to_checksum_address("0x4c9EDD5852cd905f086C759E8383e09bff1E68B3"),
     Chain.SEPOLIA: Web3.to_checksum_address("0x9458caaca74249abbe9e964b3ce155b98ec88ef2"),
 })
 
 SUSDE_TOKEN_ADDRESS_FOR_L2 = Web3.to_checksum_address("0x211Cc4DD073734dA055fbF44a2b4667d5E5fE5d2")
-SUSDE_TOKEN_ADDRESS_BY_CHAIN = defaultdict(
-    lambda: SUSDE_TOKEN_ADDRESS_FOR_L2, {
+SUSDE_TOKEN_ADDRESS_BY_CHAIN = defaultdict(lambda: SUSDE_TOKEN_ADDRESS_FOR_L2, {
     Chain.ETHEREUM: Web3.to_checksum_address("0x9d39a5de30e57443bff2a8307a4256c8797a3497"),
 })
