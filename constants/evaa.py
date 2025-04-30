8fd8a53e4c3e4817e1c10f7446328af50cae6c22
Danil Shaymurzin ⚡️ <im@murka.me>
2025-04-30 21:49:09 +0500

    fix: use actual eth blockid
    Co-authored-by: FJ-Riveros <fran.etnies@gmail.com>

--- constants/evaa.py
+++ constants/evaa.py
@@ -1,6 +1,6 @@
 from constants.integration_token import Token
 
-EVAA_USDE_START_BLOCK = 52132128 # TODO: Replace with the actual start block number
+EVAA_USDE_START_BLOCK=22382834
 
 EVAA_ENDPOINT="https://api.evaa.space" 
 
