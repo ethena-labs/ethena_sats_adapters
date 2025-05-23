d083954a194594af9891154ae3357c896cdb42be
sriyantra <85423375+sriyantra@users.noreply.github.com>
2025-05-23 06:47:38 -0600

    Add Sentiment Protocol USDE SuperPool integration on HyperEVM

---
+++ constants/sentiment.py
@@ -0,0 +1,9 @@
+"""
+Constants for Sentiment Protocol on HyperEVM.
+"""
+
+# Block at which the Sentiment USDE SuperPool contract was deployed
+SENTIMENT_USDE_SUPERPOOL_START_BLOCK = 3564320
+
+# SuperPool contract address for USDE
+SENTIMENT_USDE_SUPERPOOL_ADDRESS = "0xe45E7272DA7208C7a137505dFB9491e330BF1a4e" 
--- constants/summary_columns.py
+++ constants/summary_columns.py
@@ -61,6 +61,9 @@ class SummaryColumn(Enum):
 
     VENUS_SUSDE_PTS = ("venus_susde_pts", SummaryColumnType.ETHENA_PTS)
 
+    # Sentiment
+    SENTIMENT_USDE_PTS = ("sentiment_usde_pts", SummaryColumnType.ETHENA_PTS)
+
 
     def __init__(self, column_name: str, col_type: SummaryColumnType):
         self.column_name = column_name
