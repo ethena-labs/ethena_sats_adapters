f5a83c062bb7d4e225bd4f4c5b99d9c50618540e
andy-lin-tkspring <andy.lin@tkspring.com>
2025-07-21 16:37:33 +0800

    Remove token_to_config_map of sUSDe

--- constants/termmax.py
+++ constants/termmax.py
@@ -8,12 +8,6 @@ CHAIN_TO_CONFIG_MAP = {
         "data_manager_api_origin": "https://data-manager-api.termmax.ts.finance",
         "chain_id": "1",
         "token_to_config_map": {
-            Token.SUSDE: {
-                "address": Web3.to_checksum_address(
-                    "0x9d39a5de30e57443bff2a8307a4256c8797a3497"
-                ),
-                "start_block": 22678822, # market deploy tx: https://etherscan.io/tx/0xc26c8ebf4ece31273475024f644d5337f13244018e096a4054925a12a909f75b
-            },
             Token.PT_SUSDE_25SEP2025: {
                 "address": Web3.to_checksum_address(
                     "0x9F56094C450763769BA0EA9Fe2876070c0fD5F77"
